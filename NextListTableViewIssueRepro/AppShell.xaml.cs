9b30b4f95599d5cbeae2b3337f0c4a920042c02b
TJ Lambert <tjlambert@microsoft.com>
2023-02-07 13:12:57 -0600

    Adding ListTableView issue

---
+++ NextListTableViewIssueRepro/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NextListTableViewIssueRepro;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ NextListTableViewIssueRepro/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NextListTableViewIssueRepro;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
+
